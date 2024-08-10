2001dca1580107a2a348aa7bbf51ff10d2dc620c
Christian Ahrens <christianahrens@me.com>
2024-08-10 22:38:38 +0200

    Added app quit button

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -35,12 +35,13 @@ public:
     {
         auto bounds = getLocalBounds().toFloat();
 
+        g.setColour(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+        g.fillRect(bounds);
+
         g.setColour(getLookAndFeel().findColour(juce::TextButton::ColourIds::textColourOnId));
         
         auto barBounds = bounds.reduced(1);
         g.fillRect(barBounds.removeFromLeft(barBounds.getWidth() * (float(m_loadPercent) / 100.0f)));
-
-        g.drawRect(bounds, 1.0f);
 
         g.drawText(juce::String("Load ") + juce::String(m_loadPercent) + juce::String("%"), bounds, juce::Justification::centred);
     };
@@ -56,6 +57,20 @@ private:
     int m_loadPercent = 0;
 };
 
+//==============================================================================
+class EmptySpace :public juce::Component
+{
+public:
+    EmptySpace() : juce::Component::Component() {}
+    ~EmptySpace() {}
+
+    //==============================================================================
+    void paint(Graphics& g) override
+    {
+        g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    };
+};
+
 MainComponent::MainComponent()
     : juce::Component()
 {
@@ -63,7 +78,8 @@ MainComponent::MainComponent()
     m_mbm->onSizeChangeRequested = [=](juce::Rectangle<int> requestedSize) { setSize(requestedSize.getWidth(), requestedSize.getHeight() + 26); };
     addAndMakeVisible(m_mbm->getUIComponent());
 
-    m_setupToggleButton = std::make_unique<TextButton>("Audio Device Setup");
+    m_setupToggleButton = std::make_unique<juce::DrawableButton>("Audio Device Setup", juce::DrawableButton::ButtonStyle::ImageFitted);
+    m_setupToggleButton->setTooltip("Audio Device Setup");
     m_setupToggleButton->onClick = [this] {
         auto setupComponent = m_mbm->getDeviceSetupComponent();
         if (setupComponent)
@@ -85,6 +101,16 @@ MainComponent::MainComponent()
     };
     addAndMakeVisible(m_setupToggleButton.get());
 
+    m_powerButton = std::make_unique<juce::DrawableButton>("Quit application", juce::DrawableButton::ButtonStyle::ImageFitted);
+    m_powerButton->setTooltip("Quit application");
+    m_powerButton->onClick = [this] {
+        juce::JUCEApplication::getInstance()->quit();
+    };
+    addAndMakeVisible(m_powerButton.get());
+
+    m_emptySpace = std::make_unique<EmptySpace>();
+    addAndMakeVisible(m_emptySpace.get());
+
     m_sysLoadBar = std::make_unique<LoadBar>();
     m_mbm->onCpuUsageUpdate = [=](int loadPercent) { m_sysLoadBar->setLoadPercent(loadPercent); };
     addAndMakeVisible(m_sysLoadBar.get());
@@ -99,29 +125,29 @@ MainComponent::~MainComponent()
 
 void MainComponent::paint(Graphics &g)
 {
-    g.fillAll(getLookAndFeel().findColour(AlertWindow::backgroundColourId).darker());
-    
-    auto safeBounds = getLocalBounds();
-
-    auto setupAreaBounds = safeBounds.removeFromTop(26);
-    g.setColour(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
-    g.fillRect(setupAreaBounds);
+    g.fillAll(getLookAndFeel().findColour(juce::AlertWindow::backgroundColourId).darker());
 }
 
 void MainComponent::resized()
 {
     auto safeBounds = getLocalBounds();
 
-    auto margin = 3;
-    auto setupAreaBounds = safeBounds.removeFromTop(26).reduced(margin);
+    auto margin = 1;
+    auto setupElementArea = safeBounds.removeFromTop(26);
     auto contentAreaBounds = safeBounds;
     contentAreaBounds.removeFromTop(1);
 
-    auto setupElementArea = setupAreaBounds.removeFromTop(20);
+    if (m_powerButton)
+        m_powerButton->setBounds(setupElementArea.removeFromRight(setupElementArea.getHeight()));
+    setupElementArea.removeFromRight(margin);
     if (m_setupToggleButton)
-        m_setupToggleButton->setBounds(setupElementArea.removeFromRight(100));
+        m_setupToggleButton->setBounds(setupElementArea.removeFromRight(setupElementArea.getHeight()));
+    setupElementArea.removeFromRight(margin);
     if (m_sysLoadBar)
         m_sysLoadBar->setBounds(setupElementArea.removeFromLeft(100));
+    setupElementArea.removeFromLeft(margin);
+    if (m_emptySpace)
+        m_emptySpace->setBounds(setupElementArea);
 
     auto MenuBarMatrixComponent = m_mbm->getUIComponent();
     if (MenuBarMatrixComponent)
@@ -134,13 +160,26 @@ void MainComponent::darkModeSettingChanged()
     {
         // go dark
         m_lookAndFeel = std::make_unique<JUCEAppBasics::CustomLookAndFeel>(JUCEAppBasics::CustomLookAndFeel::PS_Dark);
-        Desktop::getInstance().setDefaultLookAndFeel(m_lookAndFeel.get());
+        juce::Desktop::getInstance().setDefaultLookAndFeel(m_lookAndFeel.get());
     }
     else
     {
         // go light
         m_lookAndFeel = std::make_unique<JUCEAppBasics::CustomLookAndFeel>(JUCEAppBasics::CustomLookAndFeel::PS_Light);
-        Desktop::getInstance().setDefaultLookAndFeel(m_lookAndFeel.get());
+        juce::Desktop::getInstance().setDefaultLookAndFeel(m_lookAndFeel.get());
     }
+
+    lookAndFeelChanged();
+}
+
+void MainComponent::lookAndFeelChanged()
+{
+    auto powerDrawable = juce::Drawable::createFromSVG(*juce::XmlDocument::parse(BinaryData::power_settings_24dp_svg).get());
+    powerDrawable->replaceColour(juce::Colours::black, getLookAndFeel().findColour(juce::TextButton::ColourIds::textColourOnId));
+    m_powerButton->setImages(powerDrawable.get());
+
+    auto setupToggleDrawable = juce::Drawable::createFromSVG(*juce::XmlDocument::parse(BinaryData::tune_24dp_svg).get());
+    setupToggleDrawable->replaceColour(juce::Colours::black, getLookAndFeel().findColour(juce::TextButton::ColourIds::textColourOnId));
+    m_setupToggleButton->setImages(setupToggleDrawable.get());
 }
 
