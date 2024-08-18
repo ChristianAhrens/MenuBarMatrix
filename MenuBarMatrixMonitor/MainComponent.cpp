d7566697bd4d0975ccca673537107b94329b8a6b
Christian Ahrens <christianahrens@me.com>
2024-08-18 15:03:04 +0200

    Added empty MenuBarMatrixMonitor project

---
+++ MenuBarMatrixMonitor/MainComponent.cpp
@@ -0,0 +1,47 @@
+/* Copyright (c) 2024, Christian Ahrens
+ *
+ * This file is part of MenuBarMatrix <https://github.com/ChristianAhrens/MenuBarMatrix>
+ *
+ * This tool is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This tool is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this tool; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "MainComponent.h"
+
+#include <CustomLookAndFeel.h>
+
+
+MainComponent::MainComponent()
+    : juce::Component()
+{
+
+    setSize(400, 350);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+void MainComponent::paint(Graphics &g)
+{
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::ColourIds::backgroundColourId));
+
+    g.setColour(getLookAndFeel().findColour(juce::TextButton::ColourIds::textColourOnId));
+    g.drawText(juce::JUCEApplication::getInstance()->getApplicationName(), getLocalBounds().toFloat(), juce::Justification::centred);
+}
+
+void MainComponent::resized()
+{
+
+}
+
