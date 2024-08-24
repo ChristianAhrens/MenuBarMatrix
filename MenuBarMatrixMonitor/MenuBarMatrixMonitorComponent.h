6f3cf7686f3e4f948d2a3ccb9616b78567d6d4b1
Christian Ahrens <christianahrens@me.com>
2024-08-24 10:28:04 +0200

    Added initial connection attemt visualization

--- MenuBarMatrixMonitor/MenuBarMatrixMonitorComponent.h
+++ MenuBarMatrixMonitor/MenuBarMatrixMonitorComponent.h
@@ -21,8 +21,46 @@
 #include <JuceHeader.h>
 
 
-class MenuBarMatrixMonitorComponent :   public juce::Component, juce::MessageListener
+class MenuBarMatrixMonitorComponent :   public juce::Component, juce::MessageListener, juce::Timer
 {
+public:
+    enum RunningStatus
+    {
+        Inactive,
+        Standby,
+        Active
+    };
+
+    struct StartupRunningIndicator
+    {
+        static constexpr auto pi = MathConstants<float>::pi;
+        static constexpr auto arcStart = pi / 2.0f;
+
+        ~StartupRunningIndicator() = default;
+        void paint(Graphics& g)
+        {
+            Path p;
+            p.addCentredArc(centre.getX(),
+                centre.getY(),
+                radius,
+                radius,
+                0.0f,
+                arcStart,
+                arcStart + 2.0f * pi * (float(progress) / 100.0f),
+                true);
+
+            g.setColour(colour);
+            g.strokePath(p, { thickness, PathStrokeType::mitered });
+        }
+
+        int progress = 0; // -1 for failure, 0-100 for actual progress
+        juce::Point<float> centre;
+        juce::Colour colour = juce::Colours::white;
+
+    private:
+        float radius = 0.0f, thickness = 0.0f;
+    };
+
 public:
     MenuBarMatrixMonitorComponent();
     ~MenuBarMatrixMonitorComponent() override;
@@ -35,10 +73,23 @@ public:
     void handleMessage(const Message& message) override;
 
     //========================================================================*
+    void timerCallback() override;
+
+    //========================================================================*
     void setRunning(bool running);
 
 private:
-    bool m_isRunning = false;
+    //========================================================================*
+    void updateStartupAnimation();
+    void showStartupTimeout();
+
+    //========================================================================*
+    RunningStatus m_runningStatus = RunningStatus::Inactive;
+    int m_startRunningAttemptTime = 0;
+    static constexpr int sc_startRunningRefreshInterval = 50; // 50ms update rate
+    static constexpr int sc_startRunningTimeout = 4000; // 4s running before attempt is considered failed
+
+    StartupRunningIndicator m_startRunningIndicator;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MenuBarMatrixMonitorComponent)
 };
